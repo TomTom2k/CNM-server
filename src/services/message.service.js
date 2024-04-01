335f48dff873e4232565467779244818ac8bd690
ttus612 <thanhtu6122002@gmail.com>
2024-04-01 15:16:01 +0700

    fix conflicts

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -1,10 +1,9 @@
 require("dotenv").config()
-const {s3} = require("../configs/aws.config")
+const { s3 } = require("../configs/aws.config")
 const ConversationModel = require('../models/conversation.model');
 const MessageModel = require('../models/message.model');
 const { io, getReceiverSocketId } = require('../socket/socket');
 
-// const s3 = new AWS.S3();
 
 const sendMessageService = async (senderId, data, files) => {
     const { conversationId, content, type } = data;
@@ -13,27 +12,27 @@ const sendMessageService = async (senderId, data, files) => {
     // Lấy thông tin cuộc trò chuyện
     let conversation = await ConversationModel.get(conversationId);
     if (!conversation) {
-        return { 
-            message: 'Cuộc hội thoại không tồn tại', 
-            status: 400, 
-            data: {} 
+        return {
+            message: 'Cuộc hội thoại không tồn tại',
+            status: 400,
+            data: {}
         };
     }
 
     if (type === "image") {
-        for(const file of files) {
+        for (const file of files) {
             // Lưu từng image vào S3 và lấy ra image url
             const image = file?.originalname.split(".");
             const fileType = image[image.length - 1];
             const filePath = `img_${Date.now().toString()}.${fileType}`;
-    
+
             const paramsS3 = {
                 Bucket: process.env.S3_BUCKET_NAME,
                 Key: filePath,
                 Body: file.buffer,
                 ContentType: file.mimetype,
             };
-    
+
             const data = await s3.upload(paramsS3).promise();
             imageURL += data.Location + " "
         }
@@ -46,9 +45,9 @@ const sendMessageService = async (senderId, data, files) => {
         content: content || imageURL.trim(),
         type
     });
-    if(type === "text"){
+    if (type === "text") {
         conversation.lastMessage = content
-    } else if(type === "image"){
+    } else if (type === "image") {
         conversation.lastMessage = "Hình ảnh"
     }
 
@@ -86,9 +85,9 @@ const getMessagesService = async (data) => {
         (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
     );
 
-    return { 
-        data: messageArray, 
-        status: 200 
+    return {
+        data: messageArray,
+        status: 200
     };
 }
 
